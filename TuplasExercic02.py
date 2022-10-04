550e2684c258486be3e4b21070ff810ab5165db4
Sung- <Vitorhugomoa99@hotmail.com>
2022-10-04 09:51:59 -0300

    Exercise02

---
+++ TuplasExercic02.py
@@ -0,0 +1,17 @@
+print("="*60)
+print("CAMPEONATO BRASILEIRO DE FUTEBOL")
+print("-"*60)
+enumerador = int(0)
+Times = ("Cruzeiro", 'Atletico', 'America', 'Flamengo', 'Corinthias',
+           'Sao Paulo', 'Goias', 'Bota Fogo', 'Boca Junior', 'Gremio',
+           'Chapecoense', 'Parana', 'Fluminense', 'Coritiba', 'Santos',
+           'Avai', 'Internacional', 'Juventude', 'Ceara', 'Congonhas')
+print("="*10, "OS 5 PRIMEIROS SÃO", "="*10)
+print(Times[0:5])
+print("-"*60)
+print("="*10, "OS 4 ULTIMOS SAO", "="*10)
+print(Times[16:20])
+print("-"*60)
+print("="*10, "OS TIMES EM ORDEM ALFABETICA", "="*10)
+print(sorted(Times))
+print("-"*60)
